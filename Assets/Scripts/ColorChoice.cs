cd01eebb3df43a7531adaa52ed7677612f9df2cd
MatteoBenaissa <matteo.benaissa@lacatholille.fr>
2022-05-22 00:39:46 +0200

    minor changes

--- Assets/Scripts/ColorChoice.cs
+++ Assets/Scripts/ColorChoice.cs
@@ -19,20 +19,20 @@ public class ColorChoice : MonoBehaviour
     }
 
     private static Hashtable hueColourValues = new Hashtable{
-         { HueColorNames.Lime,     new Color32( 166 , 254 , 0, 1 ) },
-         { HueColorNames.Green,     new Color32( 0 , 254 , 111, 1 ) },
-         { HueColorNames.Aqua,     new Color32( 0 , 201 , 254, 1 ) },
-         { HueColorNames.Blue,     new Color32( 0 , 122 , 254, 1 ) },
-         { HueColorNames.Navy,     new Color32( 60 , 0 , 254, 1 ) },
-         { HueColorNames.Purple, new Color32( 143 , 0 , 254, 1 ) },
-         { HueColorNames.Pink,     new Color32( 232 , 0 , 254, 1 ) },
-         { HueColorNames.Red,     new Color32( 254 , 9 , 0, 1 ) },
-         { HueColorNames.Orange, new Color32( 254 , 161 , 0, 1 ) },
-         { HueColorNames.Yellow, new Color32( 254 , 224 , 0, 1 ) },
+         { HueColorNames.Lime,     new Color( 166 , 254 , 0, 1 ) },
+         { HueColorNames.Green,     new Color( 0 , 254 , 111, 1 ) },
+         { HueColorNames.Aqua,     new Color( 0 , 201 , 254, 1 ) },
+         { HueColorNames.Blue,     new Color( 0 , 122 , 254, 1 ) },
+         { HueColorNames.Navy,     new Color( 60 , 0 , 254, 1 ) },
+         { HueColorNames.Purple,    new Color( 143 , 0 , 254, 1 ) },
+         { HueColorNames.Pink,     new Color( 232 , 0 , 254, 1 ) },
+         { HueColorNames.Red,     new Color( 254 , 9 , 0, 1 ) },
+         { HueColorNames.Orange, new Color( 254 , 161 , 0, 1 ) },
+         { HueColorNames.Yellow, new Color( 254 , 224 , 0, 1 ) },
      };
 
-    public static Color32 HueColourValue(HueColorNames color)
+    public static Color HueColourValue(HueColorNames color)
     {
-        return (Color32)hueColourValues[color];
+        return (Color)hueColourValues[color];
     }
 }
