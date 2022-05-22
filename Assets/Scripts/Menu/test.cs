58b7b42e36783d5299c1f0098248da44a9c1d7fc
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2022-05-23 01:17:14 +0200

    Prog Scene LevelMenu terminégros

--- Assets/Scripts/Menu/test.cs
+++ Assets/Scripts/Menu/test.cs
@@ -1,130 +1,185 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour
 {
     public Transform[] target;
     public int actualPos;
-    public bool _canMove;
 
+    Animator _anim;
+
+    public bool canMove;
+    public bool actu2;
+
+    public GameObject slider;
+
+    private void Start()
+    {
+        canMove = true;
+        _anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         float step = 3 * Time.deltaTime;
 
-        if (actualPos == 0 && _canMove)
+        #region Input
+
+        if (actualPos == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actualPos = 1;
+                transform.LookAt(target[1]);
+                StartCoroutine(Waiting());
+            }
+        }
+        if (actualPos == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actu2 = true;
+                StartCoroutine(Actual2());
+                actualPos = 2;
+                transform.LookAt(target[2]);
+                StartCoroutine(Waiting());
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actualPos = 0;
+                transform.LookAt(target[0]);
+                StartCoroutine(Waiting());
+            }
+        }
+        if (actualPos == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && canMove == true)
+            {
+                canMove = false;
+                actu2 = false;
+                _anim.SetBool("isMouving", true);
+                actualPos = 3;
+                transform.LookAt(target[3]);
+                StartCoroutine(Waiting());
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actu2 = false;
+                actualPos = 1;
+                transform.LookAt(target[1]);
+                StartCoroutine(Waiting());
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && actu2 == false && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actu2 = false;
+                actualPos = 4;
+                transform.LookAt(target[4]);
+                StartCoroutine(Waiting());
+            }
+        }
+        if (actualPos == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actualPos = 2;
+                transform.LookAt(target[2]);
+                StartCoroutine(Waiting());
+            }
+        }
+        if(actualPos == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow) && canMove == true)
+            {
+                canMove = false;
+                _anim.SetBool("isMouving", true);
+                actualPos = 2;
+                transform.LookAt(target[2]);
+                StartCoroutine(Waiting());
+            }
+        }
+
+        #endregion
+
+        #region Mouvement
+        if (actualPos == 0)
         {
             transform.position = Vector3.MoveTowards(transform.position, target[0].position, step);
         }
-        if (actualPos == 1 && _canMove)
+        if (actualPos == 1)
         {
             transform.position = Vector3.MoveTowards(transform.position, target[1].position, step);
         }
-        if (actualPos == 2 && _canMove)
+        if (actualPos == 2)
         {
             transform.position = Vector3.MoveTowards(transform.position, target[2].position, step);
         }
-        if (actualPos == 3 && _canMove)
+        if (actualPos == 3)
         {
             transform.position = Vector3.MoveTowards(transform.position, target[3].position, step);
         }
-        if (actualPos == 4 && _canMove)
+        if (actualPos == 4)
         {
             transform.position = Vector3.MoveTowards(transform.position, target[4].position, step);
         }
+        #endregion
 
-
-        //ActualPos = 0
-        if (actualPos == 0)
+        #region EnterInLevel
+        if (actualPos == 1 && canMove == true && Input.GetKeyDown(KeyCode.Return))
         {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                actualPos = 1;
-                transform.LookAt(target[1]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
+            StartCoroutine(Switch());
         }
-
-        //ActualPos = 1
-        if (actualPos == 1)
+        if (actualPos == 3 && canMove == true && Input.GetKeyDown(KeyCode.Return))
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                actualPos = 2;
-                transform.LookAt(target[2]);
-                _canMove = true;
-                StartCoroutine(Moving());
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                actualPos = 0;
-                transform.LookAt(target[0]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
+            StartCoroutine(Switch());
         }
-
-        //ActualPos = 2
-        if (actualPos == 2)
+        if (actualPos == 4 && canMove == true && Input.GetKeyDown(KeyCode.Return))
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                actualPos = 3;
-                transform.LookAt(target[3]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
-
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                actualPos = 1;
-                transform.LookAt(target[1]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                actualPos = 4;
-                transform.LookAt(target[4]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
+            StartCoroutine(Switch());
         }
-
-        //ActualPos = 3
-        if (actualPos == 3)
-        {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                actualPos = 2;
-                transform.LookAt(target[2]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
-        }
-
-        //ActualPos = 4
-        if(actualPos == 4)
-        {
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                actualPos = 2;
-                transform.LookAt(target[2]);
-                _canMove = true;
-                StartCoroutine(Moving());
-            }
-        }
-
+        #endregion
     }
 
-    IEnumerator Moving()
+    #region Coroutine
+    IEnumerator Actual2()
     {
-        yield return new WaitForSeconds(3);
-        _canMove = false;
+        yield return new WaitForSeconds(0.1f);
+        actu2 = false;
+    }
+
+    IEnumerator Waiting()
+    {
+        yield return new WaitForSeconds(1.5f);
+        canMove = true;
+        _anim.SetBool("isMouving", false);
+    }
+    #endregion
+
+    IEnumerator Switch()
+    {
+        slider.SetActive(true);
+        yield return new WaitForSeconds(2);
+        if(actualPos == 1)
+            SceneManager.LoadScene(2);
+        if (actualPos == 3)
+            SceneManager.LoadScene(2);
+        if (actualPos == 4)
+            SceneManager.LoadScene(2);
     }
 }
