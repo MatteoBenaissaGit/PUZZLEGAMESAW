084ac37d47c3ed598c66f22038096e3cf605af5d
MatteoBenaissa <matteo.benaissa@lacatholille.fr>
2022-05-22 21:07:02 +0200

    boutonA

--- Assets/Scripts/GPEs/Disjoncteur.cs
+++ Assets/Scripts/GPEs/Disjoncteur.cs
@@ -8,29 +8,41 @@ public class Disjoncteur : ClassActivator
     Animator _anim;
     Animator _characterAnim;
     Collider _characterCollider;
+    
+    [Header("Button&Cam Reference")][Space(10)]
+    public GameObject ButtonImage;
+    public Transform Camera;
 
     private void Start()
     {
         _anim = GetComponent<Animator>();
         _characterAnim = Character.GetComponent<Animator>();
         _characterCollider = Character.GetComponent<Collider>();
+        ButtonImage.SetActive(false);
     }
 
     private void Update()
     {
         PushCheck();
+        ButtonImage.transform.LookAt(Camera);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other == _characterCollider)
+        {
             _isTrigger = true;
+            ButtonImage.SetActive(true);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other == _characterCollider)
+        {
             _isTrigger = false;
+            ButtonImage.SetActive(false);
+        }
     }
 
     void PushCheck()
