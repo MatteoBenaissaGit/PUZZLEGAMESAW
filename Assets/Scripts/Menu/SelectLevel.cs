753cb0d9d9d579cd8e179df4168ea619545c127d
MatteoBenaissa <matteo.benaissa@lacatholille.fr>
2022-05-24 23:11:48 +0200

    Revert "corrections de LD... encore...."
    This reverts commit 8cafcb075636e998e28e2381e4a8e93ea37305c3.

--- Assets/Scripts/Menu/SelectLevel.cs
+++ Assets/Scripts/Menu/SelectLevel.cs
@@ -29,7 +29,6 @@ public class SelectLevel : MonoBehaviour
 
     private void Update()
     {
-
         for (int i = 1; i <= Buttons.Length; i++)
         {
             if(i <= playerLevelScene.levelUnlock)
@@ -51,9 +50,7 @@ public class SelectLevel : MonoBehaviour
 
         if(playerLevelScene.selec == true)
         {
-            var hor = Input.GetAxis("Horizontal");
-            
-            if (Input.GetKeyDown(KeyCode.RightArrow) || hor > 0.5)
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 if(playerLevelScene.levelUnlock % 2 == 0)
                 {
@@ -95,7 +92,7 @@ public class SelectLevel : MonoBehaviour
                 }
             }
 
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || hor < -0.5)
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 if (playerLevelScene.levelUnlock % 2 == 0)
                 {
@@ -141,17 +138,17 @@ public class SelectLevel : MonoBehaviour
 
         if(selected == 1)
         {
-            if(playerLevelScene.actualPos == 1 && playerLevelScene.levelUnlock >= 5 && SelectButton() && playerLevelScene.canPick == true)
+            if(playerLevelScene.actualPos == 1 && playerLevelScene.levelUnlock >= 5 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 5;
                 StartCoroutine(Switch());
             }
-            if (playerLevelScene.actualPos == 3 && SelectButton() && playerLevelScene.canPick == true)
+            if (playerLevelScene.actualPos == 3 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 1;
                 StartCoroutine(Switch());
             }
-            if (playerLevelScene.actualPos == 4 && playerLevelScene.levelUnlock >= 3 && SelectButton() && playerLevelScene.canPick == true)
+            if (playerLevelScene.actualPos == 4 && playerLevelScene.levelUnlock >= 3 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 3;
                 StartCoroutine(Switch());
@@ -159,29 +156,22 @@ public class SelectLevel : MonoBehaviour
         }
         if (selected == 2)
         {
-            if (playerLevelScene.actualPos == 1 && playerLevelScene.levelUnlock >= 6 && SelectButton() && playerLevelScene.canPick == true)
+            if (playerLevelScene.actualPos == 1 && playerLevelScene.levelUnlock >= 6 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 6;
                 StartCoroutine(Switch());
             }
-            if (playerLevelScene.actualPos == 3 && playerLevelScene.levelUnlock >= 2 && SelectButton() && playerLevelScene.canPick == true)
+            if (playerLevelScene.actualPos == 3 && playerLevelScene.levelUnlock >= 2 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 2;
                 StartCoroutine(Switch());
             }
-            if (playerLevelScene.actualPos == 4 && playerLevelScene.levelUnlock >= 4 && SelectButton() && playerLevelScene.canPick == true)
+            if (playerLevelScene.actualPos == 4 && playerLevelScene.levelUnlock >= 4 && Input.GetKeyDown(KeyCode.Return) && playerLevelScene.canPick == true)
             {
                 switchLevel = 4;
                 StartCoroutine(Switch());
             }
         }
-    }
-
-    bool SelectButton()
-    {
-        if (Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("ButtonActivation"))
-            return true;
-        else return false;
     }
 
     IEnumerator Switch()
